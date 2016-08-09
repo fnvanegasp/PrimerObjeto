1083ece8ca77b10178baf21591bd412385daf81d
Estudiante <Estudiante@10.203.1.221>
2016-08-09 14:51:13 -0500

    ejercicio

---
+++ Ejercicio/src/ejercicio/Ejercicio.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+
+int x=3;
+ while (x > 0){
+
+     if (x > 2) {
+ System.out.print("a");
+}
+     
+x = x - 1; 
+
+
+if ( x == 2) {
+ System.out.print ("b c");
+ }
+
+
+if (x == 1){
+ System.out.print("d");
+  x = x - 1;
+}
+
+
+
+System.out.print ("-");
+        // TODO code application logic here
+    }
+    }
+}
+    
+
